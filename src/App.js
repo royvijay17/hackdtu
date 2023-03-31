dba8ddf78942f738432178ccd65101109a682cea
vikrant <viksingh465@gmail.com>
2023-03-31 14:28:40 +0530

    start page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import Start from '../src/component/start' 
+import {Routes, Route} from 'react-router-dom';
+import {useState} from 'react';
 
 function App() {
+  const [username,setUsername]=useState("");
   return (
+    
     <div className="App">
-      <div className="nav">
+      <Routes>
+        <Route path="/" element={<Start setUsername={setUsername} username={username}/>}/>
+        <Route path="/home" element={
+          <>
+        <div className="nav">
         <div className="logo">COMPANY</div>
         <div className="rightnav">
           <div botton ="Home">Home</div>
@@ -13,12 +22,16 @@ function App() {
           < div botton ='About'>About</div>
           < div botton ='Contact'>Contact</div>
         </div>
-        <div className="body">
-          new body
-        </div>
       </div>
+      <div className="newbody">
+        hello there
+      </div>
+     </>
+        }/>
+      </Routes>
+      </div>
+      
 
-    </div>
   );
 }
 
